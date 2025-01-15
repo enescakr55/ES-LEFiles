379ac649a081ff10bf3f22f7a8d2d836ada11d7c
enescakr55 <enescakr01@hotmail.com>
2025-01-15 21:08:27 +0300

    #15 - Karanlık tema

--- frontend/lefilesUi/projects/ui/src/app/components/navigation/horizontal-menu/horizontal-menu.component.ts
+++ frontend/lefilesUi/projects/ui/src/app/components/navigation/horizontal-menu/horizontal-menu.component.ts
@@ -10,38 +10,51 @@ import { AuthService } from '../../../services/auth.service';
   styleUrls: ['./horizontal-menu.component.css']
 })
 export class HorizontalMenuComponent implements OnInit {
-  currentPath:string = location.pathname;
-  menuItems:MenuItemModel[] = [];
-  constructor(private activatedRoute:ActivatedRoute,private router:Router,private storageService:StorageService,private authService:AuthService) { }
+  currentPath: string = location.pathname;
+  menuItems: MenuItemModel[] = [];
+  darkTheme: boolean = false;
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private storageService: StorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.pathListener();
     this.createMenuItems();
   }
-  pathListener(){
+  switchTheme(theme: string) {
+    var container = document.getElementsByClassName("ui-container")[0];
+    if (theme == "dark") {
+      container.classList.remove("theme-dark");
+      container.classList.remove("theme-light");
+      container.classList.add("theme-dark");
+    } else if (theme == "light") {
+      container.classList.remove("theme-dark");
+      container.classList.remove("theme-light");
+      container.classList.add("theme-light");
+    }
+  }
+  pathListener() {
     this.router.events.subscribe({
-      next:(param)=>{
-       if(param instanceof NavigationEnd){
-         this.currentPath = param.url;
-       } 
+      next: (param) => {
+        if (param instanceof NavigationEnd) {
+          this.currentPath = param.url;
+        }
       }
     })
   }
   createMenuItems() {
     this.menuItems = [
       {
-        labelKey:"menuItem.main",
-        path:"/main",
-        icon:"bi bi-house-door-fill"
+        labelKey: "menuItem.main",
+        path: "/main",
+        icon: "bi bi-house-door-fill"
       },
       {
-        labelKey:"menuItem.files",
-        path:"/files",
-        icon:"bi bi-folder-fill"
+        labelKey: "menuItem.files",
+        path: "/files",
+        icon: "bi bi-folder-fill"
       }
     ]
   }
-  logout(){
+  logout() {
     this.authService.logout();
   }
 
