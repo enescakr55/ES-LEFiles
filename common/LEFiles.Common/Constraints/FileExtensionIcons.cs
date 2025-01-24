fc76f58d592abc6a03360369ede00d73032df880
enescakr55 <enescakr01@hotmail.com>
2025-01-24 16:42:39 +0300

    ...

---
+++ common/LEFiles.Common/Constraints/FileExtensionIcons.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEFiles.Common.Constraints
+{
+  public class FileExtensionIconModel
+  {
+    public string[] Extensions { get; set; } = new string[0];
+    public string Icon { get; set; } = string.Empty;
+  }
+  public static class FileExtensionIcons
+  {
+    public static List<FileExtensionIconModel> Icons = new List<FileExtensionIconModel>
+    {
+      new FileExtensionIconModel {
+        Extensions = FileExtensionTypes.Images.ToArray(),
+        Icon = "bi bi-image-fill"
+      },
+      new FileExtensionIconModel {
+        Extensions = FileExtensionTypes.Audios.ToArray(),
+        Icon = "bi bi-file-earmark-music-fill"
+      },
+      new FileExtensionIconModel {
+        Extensions = FileExtensionTypes.Videos.ToArray(),
+        Icon = "bi bi-film"
+      },
+      new FileExtensionIconModel {
+        Extensions = new string[] {".zip",".rar",".7z",".gzip",".tar",".bzip2" },
+        Icon = "bi bi-file-zip-fill"
+      },      
+      new FileExtensionIconModel {
+        Extensions = new string[] {".iso" },
+        Icon = "bi bi-disc-fill"
+      },new FileExtensionIconModel {
+        Extensions = new string[] {".exe" },
+        Icon = "bi bi-filetype-exe"
+      }
+    };
+  }
+}
---
+++ common/LEFiles.Common/Constraints/FileExtensionTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEFiles.Common.Constraints
+{
+  public static class FileExtensionTypes
+  {
+    public static string[] Images = { ".jpg", ".jpeg", ".png", ".bmp", ".webp", ".gif", ".tiff", ".tif" };
+    public static string[] Videos = { ".mp4",".mov",".wmv",".avi",".avchd",".mkv" };
+    public static string[] Audios = {".m4a",".mp3",".wav" };
+    public static string[] Documents = { ".pdf",".xls",".xlsx",".docx",".doc",".ppt",".pptx",".txt" };
+    /* Yalnızca butonların görüntülenmesi için */
+    public static string[] Viewables = { ".jpg", ".jpeg", ".png", ".bmp", ".webp", ".mp4", ".avi", ".pdf", ".txt", ".m4a", ".mp3", ".wav" };
+    public static string[] MiniPreview = { ".jpg", ".jpeg", ".png", ".bmp", ".webp" };
+    public static string[] Editables = { ".txt" };
+    //
+
+  }
+}
