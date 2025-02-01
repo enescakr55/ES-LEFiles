2a00084cc5f60f6a3526f379bc863121b069b128
enescakr55 <enescakr01@hotmail.com>
2025-02-02 01:33:07 +0300

    #7 - Dosyanın yeniden adlandırılması için gerekli endpoint hazırlandı

---
+++ backend/LEFiles/LEFiles/LEFiles.Services/ServiceModels/UserInterface/Files/Requests/RenameFileRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEFiles.Services.ServiceModels.UserInterface.Files.Requests
+{
+  public class RenameFileRequest
+  {
+    public string FileId { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+  }
+}
